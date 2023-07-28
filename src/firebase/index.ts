cde2ec2dbfde86c7ca38125f6c079c9d66d119b7
unknown <solalex.s.a@gmail.com>
2023-07-28 13:21:59 +0300

    add host

---
+++ src/firebase/index.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4TSqk-NCHChBwvt1MX9wIRGpx6oFOeVY",
+  authDomain: "skyeng-test-6dfb3.firebaseapp.com",
+  projectId: "skyeng-test-6dfb3",
+  storageBucket: "skyeng-test-6dfb3.appspot.com",
+  messagingSenderId: "558003815164",
+  appId: "1:558003815164:web:512bfd4332bb0866dd8d79"
+};
+
+const app = initializeApp(firebaseConfig);
